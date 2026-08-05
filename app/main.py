31ab460051cb1a3a3188c5afa0c4c23eb37d03e2
khokhobla3 <ismail.guennoun01@gmail.com>
2026-08-05 21:52:03 +0100

    second commit

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from . import models
 from .routers import posts, users, authentification, votes
 from .database import engine
 from fastapi.middleware.cors import CORSMiddleware
+from .database import get_db
+
 
 #models.base.metadata.create_all(bind=engine)
 
@@ -22,3 +24,7 @@ app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(authentification.router)
 app.include_router(votes.router)
+
+@app.get("/")
+def helloworld(db : Depends(get_db)):
+    return {'hello' : ' world'}
